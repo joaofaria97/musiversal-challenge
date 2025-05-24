4cf82f53813e0ef117bafd6509ddd6424a4ca626
João Faria <joao-faria.97@hotmail.com>
2025-05-24 10:25:16 +0100

    package dependency fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@musiversal/design-system'],
+  transpilePackages: ['@musiversal/design-system', '@musiversal/api-client'],
   env: {
     PORT: process.env.PORT || '3001',
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
